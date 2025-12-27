3242d4a7afcd13465109724237089637a43fd620
AmadorCDR <amadorcasillasdr@gmail.com>
2025-12-26 22:34:08 -0600

    Error de scroll

--- src/components/products/ProductQRModal.jsx
+++ src/components/products/ProductQRModal.jsx
@@ -230,7 +230,10 @@ export const ProductQRModal = ({ isOpen, onOpenChange, product }) => {
                 Código QR del producto
               </p>
             </ModalHeader>
-            <ModalBody className="py-6 gap-6 overflow-y-auto! [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
+            <ModalBody
+              className="touch-pan-y py-6 gap-6 overflow-y-auto! [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]"
+              style={{ touchAction: "pan-y" }}
+            >
               <div className="flex flex-col items-center gap-6 w-full">
                 <div className="bg-white p-1 rounded-lg shadow-large">
                   {isLoadingQr ? (
